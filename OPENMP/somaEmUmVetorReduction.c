19df8ebb8c688e3cf48d6b39c1e25f8413f02e1c
Luiz Reginaldo G Guimaraes <luizreginaldogg@hotmail.com>
2020-06-16 21:13:36 -0300

    soma Em Um Vetor usando a diretiva Reduction

---
+++ OPENMP/somaEmUmVetorReduction.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "omp.h"
+
+#define TAM 8
+
+int main(){
+	int A[TAM] = {1,2,3,4,10,12,8,5};
+	int soma,sum =0;
+	omp_set_num_threads(4);
+	#pragma omp parallel default(shared) reduction(+:sum)
+	{
+
+		#pragma omp for 
+		for(int i=0;i<TAM;i++){
+			//int numt = omp_get_num_threads();		
+			int tid = omp_get_thread_num();
+			printf("Processor %d add %d\n",tid,A[i]);
+			soma+= A[i];		
+		}
+	}
+	printf("%d ",soma);	
+	printf("\n");
+}
