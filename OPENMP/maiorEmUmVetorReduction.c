6b12f7e6044a864e88a11285c53af4b88a34e4cb
Luiz Reginaldo G Guimaraes <luizreginaldogg@hotmail.com>
2020-06-16 21:31:58 -0300

    maior Em Um Vetor usando Reduction

--- OPENMP/maiorEmUmVetorReduction.c
+++ OPENMP/maiorEmUmVetorReduction.c
@@ -4,21 +4,26 @@
 
 #define TAM 8
 
+int max(int a, int b){
+	if(a>b)return a;
+	return b;
+}
+
 int main(){
 	int A[TAM] = {1,2,3,4,10,12,8,5};
-	int soma =0;
+	int maior = 0;
 	omp_set_num_threads(4);
-	#pragma omp parallel default(shared) reduction(+:soma)
+	#pragma omp parallel default(shared) reduction(max:maior)
 	{
 
 		#pragma omp for 
 		for(int i=0;i<TAM;i++){
 			//int numt = omp_get_num_threads();		
 			int tid = omp_get_thread_num();
-			//printf("Processor %d add %d\n",tid,A[i]);
-			soma+= A[i];		
+			maior = max(maior,A[i]);
+			printf("Processor %d maior %d\n",tid,A[i]);
 		}
 	}
-	printf("%d ",soma);	
+	printf("%d ",maior);	
 	printf("\n");
 }
